532231e00ef15a5dbde8f06ca2dbc5804aeb85c7
Eloise Lardet <eloise.lardet@gmail.com>
2024-01-26 12:05:26 +0000

    shell scripts for hpc/cluster

--- run/run_hpc.sh
+++ run/run_hpc.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-src_dir=$HOME/Code/ratchet_resetting/src
+src_dir=$HOME/ratchet_resetting/src
 
 ## Parameters
-L=1
-a=0.2
-h=1
-D=0.1
-r=0.2
+L=$1
+a=$2
+h=$3
+D=$4
+r=$5
 
-x0=0
-dt=0.001
-dt_save=1
-t=20
-samples=$((10 ** 6))
-seed=1
+x0=$6
+dt=$7
+dt_save=$8
+t=$9
+samples=${10}
+seed=${11}
 
 
-run_dir=$HOME/Code/ratchet_resetting/simulation_data/h${h}_a${a}/D${D}/r${r}
+run_dir=$HOME/ratchet_resetting/simulation_data/h${h}_a${a}/D${D}/r${r}
 if [ ! -d "$run_dir" ]; then
     mkdir -p $run_dir
     echo "Creating directory : $run_dir"
@@ -44,6 +44,6 @@ echo ${t} >> 'inpar'
 echo ${samples} >> 'inpar'
 echo ${seed} >> 'inpar'
 
-time ${src_dir}/ratchet_reset inpar
+time ${src_dir}/ratchet_reset_hpc inpar
 
 echo "Simulation completed."
